886569285f1cdfa463580f3e95c72ca95e58f40f
Kent <642238553@qq.com>
2013-12-05 16:06:23 +0800

    视频编码解码成功 	modified:   README 	new file:   practice/OpenCV/CamEncode&Decode/CMakeCache.txt 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CMakeCCompiler.cmake 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CMakeCXXCompiler.cmake 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CMakeDetermineCompilerABI_C.bin 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CMakeDetermineCompilerABI_CXX.bin 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CMakeOutput.log 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CMakeSystem.cmake 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CompilerIdC/CMakeCCompilerId.c 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CompilerIdC/a.out 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/CompilerIdCXX/a.out 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/Makefile.cmake 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/Makefile2 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/TargetDirectories.txt 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/cmake.check_cache 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/progress.marks 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/CXX.includecache 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/DependInfo.cmake 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/build.make 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/cmake_clean.cmake 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/depend.internal 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/depend.make 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/flags.make 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/link.txt 	new file:   practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/progress.make 	new file:   practice/OpenCV/CamEncode&Decode/CMakeLists.txt 	new file:   practice/OpenCV/CamEncode&Decode/Makefile 	new file:   practice/OpenCV/CamEncode&Decode/cmake.sh 	new file:   practice/OpenCV/CamEncode&Decode/cmake.sh~ 	new file:   practice/OpenCV/CamEncode&Decode/cmake_install.cmake 	new file:   practice/OpenCV/CamEncode&Decode/haarcascade_eye_tree_eyeglasses.xml 	new file:   practice/OpenCV/CamEncode&Decode/haarcascade_frontalface_alt.xml 	new file:   practice/OpenCV/CamEncode&Decode/vedio.cpp

---
+++ practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kent/桌面/tmp/smartcarwithcemera/practice/OpenCV/CamEncode&Decode/vedio.cpp" "/home/kent/桌面/tmp/smartcarwithcemera/practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/vedio.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/kent/opencv"
+  "/home/kent/opencv/include"
+  "/home/kent/opencv/include/opencv"
+  "/home/kent/opencv/modules/core/include"
+  "/home/kent/opencv/modules/flann/include"
+  "/home/kent/opencv/modules/imgproc/include"
+  "/home/kent/opencv/modules/highgui/include"
+  "/home/kent/opencv/modules/features2d/include"
+  "/home/kent/opencv/modules/calib3d/include"
+  "/home/kent/opencv/modules/ml/include"
+  "/home/kent/opencv/modules/video/include"
+  "/home/kent/opencv/modules/legacy/include"
+  "/home/kent/opencv/modules/objdetect/include"
+  "/home/kent/opencv/modules/photo/include"
+  "/home/kent/opencv/modules/gpu/include"
+  "/home/kent/opencv/modules/ocl/include"
+  "/home/kent/opencv/modules/nonfree/include"
+  "/home/kent/opencv/modules/contrib/include"
+  "/home/kent/opencv/modules/stitching/include"
+  "/home/kent/opencv/modules/superres/include"
+  "/home/kent/opencv/modules/ts/include"
+  "/home/kent/opencv/modules/videostab/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ practice/OpenCV/CamEncode&Decode/CMakeFiles/vedio.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/vedio.dir/vedio.o"
+  "vedio.pdb"
+  "vedio"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/vedio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
